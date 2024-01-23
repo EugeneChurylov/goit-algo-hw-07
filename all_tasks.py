e1c16ceb0ba0893a5cd12127566f0d24e65ba07e
EugeneChurylov <e.churylov@gmail.com>
2024-01-23 14:14:07 +0100

    first try

---
+++ all_tasks.py
@@ -0,0 +1,89 @@
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+    def __str__(self, level=0, prefix="Root: "):
+        ret = "\\t" * level + prefix + str(self.val) + "\\n"
+        if self.left:
+            ret += self.left.__str__(level + 1, "L--- ")
+        if self.right:
+            ret += self.right.__str__(level + 1, "R--- ")
+        return ret
+
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+def search(root, key):
+    if root is None or root.val == key:
+        return root
+    if key < root.val:
+        return search(root.left, key)
+    return search(root.right, key)
+
+# task 2
+def min_value_node(node):
+    current = node
+    while current.left:
+        current = current.left
+    return current.val
+
+# task 1
+def max_value_node(root):
+    current = root
+    while current.right:
+        current = current.right
+    return current.val
+
+# task 3
+def find_sum_of_values(root):
+    if root is None:
+        return 0
+    else:
+        return (find_sum_of_values(root.left) +
+                root.val +
+                find_sum_of_values(root.right))
+
+def delete(root, key):
+    if not root:
+        return root
+
+    if key < root.val:
+        root.left = delete(root.left, key)
+    elif key > root.val:
+        root.right = delete(root.right, key)
+    else:
+        if not root.left:
+            temp = root.right
+            root = None
+            return temp
+        elif not root.right:
+            temp = root.left
+            root = None
+            return temp
+        root.val = min_value_node(root.right).val
+        root.right = delete(root.right, root.val)
+    return root
+
+root = Node(5)
+root = insert(root, 3)
+root = insert(root, 2)
+root = insert(root, 4)
+root = insert(root, 7)
+root = insert(root, 6)
+root = insert(root, 8)
+
+max_value = max_value_node(root)
+print("Найбільше значення у дереві:", max_value)
+min_value = min_value_node(root)
+print("Найбільше значення у дереві:", min_value)
+sum_of_values = find_sum_of_values(root)
+print("Сума всіх значень у дереві:", sum_of_values)
---
+++ readme.md
@@ -0,0 +1,11 @@
+# task 1
+
+def max_value_node
+
+# task 2
+
+def min_value_node
+
+# task 3
+
+def find_sum_of_values
